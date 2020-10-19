c8c9b4a932fd0ec6dd4e5a5c1c2a35a326554037
shashiyuva295 <shashiyuvasoft295@gmail.com>
2020-10-19 17:39:45 +0530

    try to add search feature

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,59 +1,53 @@
 class ArticlesController < ApplicationController
-	
-	def index
-   	@articles = Article.all
-  	end
-
-	def show
-    	@article = Article.find(params[:id])
-  	end
-
-	def new
-		@article = Article.new
-	end
-
-	def edit 
-		@article = Article.find(params[:id])
-	end
-
-	def create
-  		@article = Article.new(article_params)
+  def index
+    @articles = Article.all
+  end
  
-  		if @article.save
-    		redirect_to @article
-  		else
-    		render 'new'
-  		end
-	end
-
-	def update
-		@article = Article.find(params[:id])
-		
-		if @article.update(article_params)
-			redirect_to @article
-		else 
-			render 'edit'
-		end
-	end
-
-	def destroy
-  		@article = Article.find(params[:id])
-  		@article.destroy
+  def show
+    @article = Article.find(params[:id])
+  end
  
-  		redirect_to articles_path
+  def new
+    @article = Article.new
+  end
+ 
+  def edit
+    @article = Article.find(params[:id])
+  end
+ 
+  def create
+    @article = Article.new(article_params)
+ 
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
+ 
+  def update
+    @article = Article.find(params[:id])
+ 
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+ 
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+ 
+    redirect_to articles_path
+  end
+
+  def search
+  	@articles = Article.search(params[:id])
+  end
+ 
+  private
+    def article_params
+      params.require(:article).permit(:title, :text)
+    end
 end
-	
-	#def res
-		#resource: photos
-
-
-	private
-  		def article_params
-    		params.require(:article).permit(:title, :text)
-  		end
-
-
-
-end
-
-#
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,15 @@
 class Article < ApplicationRecord
-    #validates :title,:text, presence: true,length: { minimum: 5 }
-    #validates :terms_of_service, 	:acceptance => { :accept => 'yes' }
-   # validates :title, inclusion: { in: %w(title1 title2 title3), message: "%{value} is not a valid title" }
-
+    # validates :title,:text, presence: true,length: { minimum: 5 }
+    # validates :terms_of_service, 	:acceptance => { :accept => 'yes' }
+    # validates :title, inclusion: { in: %w(title1 title2 title3), message: "%{value} is not a valid title" }
+   def self.search(search)
+   		if search
+   			  article_type = Article.find(title: search)
+			  if article_type
+				    self.where(article_id: article_type)
+		  	else
+				  @article = Article.all
+			end   	
+   		end
+   end
 end
